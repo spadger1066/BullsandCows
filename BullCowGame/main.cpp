b27ac18a9a9034c4e8882557db99774c637397c0
Stephen Maitland <s.maitland@me.com>
2016-08-03 06:31:06 +0100

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Stephen on 03/08/2016.
+//  Copyright © 2016 luminator.technology.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Welcome to Bulls and Cows\n";
+    return 0;
+}
